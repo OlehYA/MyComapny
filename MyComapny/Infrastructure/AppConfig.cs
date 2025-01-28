a08d7a31ae984bd3b4c3a0b4d03850c8ccf028a2
OlehYa <olehyakuts@gmail.com>
2025-01-28 16:32:05 +0100

    Setting up the configuration

---
+++ MyComapny/Infrastructure/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace MyComapny.Infrastructure
+{
+    public class AppConfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public Company Company { get; set; } = new Company();
+    }
+
+    public class TinyMCE
+    {
+        public string? APIKEY { get; set; }
+    }
+
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompaPhone { get; set; }
+        public string? CompanyPhoneShort { get; set; }
+        public string? CompanyEmail { get; set; }
+    }
+}
