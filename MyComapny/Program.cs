f4321acfd3d198cdc7ab4547a7bd30c800852189
OlehYa <olehyakuts@gmail.com>
2025-01-29 17:10:24 +0100

    Bundlling and Minification

--- MyComapny/Program.cs
+++ MyComapny/Program.cs
@@ -1,15 +1,41 @@
+using MyComapny.Infrastructure;
+
 namespace MyComapny
 {
     public class Program
-    {
-        public static void Main(string[] args)
+    { 
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+            // Setings in configuration file appsettings.json
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            app.Run();
+            //Cover the section Project in object form for confortable
+
+            IConfiguration configuration = configBuilder.Build();
+            AppConfig config = configuration.GetSection("Project").Get<AppConfig>()!;
+
+            //Setings functions controllers
+            builder.Services.AddControllersWithViews();
+
+            //Created configuration
+            WebApplication app = builder.Build();
+
+            //Setings utilizing static fails (js, css, other)
+            app.UseStaticFiles();
+
+            //Setings sustem routing
+            app.UseRouting();
+
+            //registration need us routing
+
+            app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+           await app.RunAsync();
         }
     }
 }
