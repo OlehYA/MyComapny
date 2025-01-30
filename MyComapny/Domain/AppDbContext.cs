2950dbd8c33ee8a925be61c3d529055411a94cf0
OlehYa <olehyakuts@gmail.com>
2025-01-30 21:39:22 +0100

    Creating a database context

---
+++ MyComapny/Domain/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyComapny.Domain.Entities;
+namespace MyComapny.Domain
+{
+    //Text DB
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<ServiceCategory> ServiceCategories { get; set; } = null!;
+        public DbSet<Service> Services { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            string adminName = "admin";
+            string roleAdminId = "458E4B9D - C41C - 4FEC - BAD0 - 8630C426BBC2";
+            string userAdminId = "7349C8A0-F78D-4307-9C39-01F59E59DC4F";
+
+            //Add role administration cate
+            builder.Entity<IdentityRole>().HasData(new IdentityRole()
+            {
+                Id = roleAdminId,
+                Name = adminName,
+                NormalizedName = adminName.ToUpper()
+            });
+
+            //Add new IdentityUser in anministration cyte
+            builder.Entity<IdentityUser>().HasData(new IdentityUser()
+            {
+                Id = userAdminId,
+                UserName = adminName,
+                NormalizedUserName = adminName.ToUpper(),
+                Email= "admin@admin.com",
+                NormalizedEmail = "admin@admin.com",
+                EmailConfirmed = true,
+                PasswordHash  = new PasswordHasher<IdentityUser>().HashPassword(new IdentityUser(), adminName),
+                PhoneNumberConfirmed = true
+            });
+
+            //Defining an admin for a certain role
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>()
+            {
+                RoleId = roleAdminId,
+                UserId = userAdminId
+            });
+
+        }
+    }
+}
